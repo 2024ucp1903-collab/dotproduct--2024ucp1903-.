74efd3ad42cfcc606e43d0f06802da8ebc12108c
POOJA <2024ucp1717@mnit.ac.in>
2025-11-17 15:08:34 +0530

    git 1

---
+++ vecaddjatin.py
@@ -0,0 +1,15 @@
+v1 = list(map(float, input("Enter the first vector (space-separated): ").split()))
+v2 = list(map(float, input("Enter the second vector (space-separated): ").split()))
+
+if len(v1) != len(v2):
+    print("Error: Vectors must have the same length.")
+else:
+    dot_product = sum(a * b for a, b in zip(v1, v2))
+    print("Dot product:", dot_product)
+
+print("baba")
+print("baba again because why not")
+print("baba part 3")
+
+
+
