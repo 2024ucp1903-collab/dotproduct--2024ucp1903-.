df8c5ee4e0f1eab451957ff4c3e6e1c5d52773c8
Aman Choudhary <2023ucp1707@mnit.ac.in>
2025-11-17 15:42:33 +0530

    Make change for KadaJatin Manchi

--- vecaddjatin.py
+++ vecaddjatin.py
@@ -5,7 +5,7 @@ if len(v1) != len(v2):
     print("Error: Vectors must have the same length.")
 else:
     dot_product = sum(a * b for a, b in zip(v1, v2))
-    print("Dot product:", dot_product)
+    print("Dot product for JatinBabe:", dot_product)
 
 print("baba")
 print("baba again because why not")
